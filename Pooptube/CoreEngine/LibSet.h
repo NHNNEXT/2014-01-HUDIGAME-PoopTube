d4356354cfcbd49de33ad78f644a6e77c26545a7
양현찬 <e7315126@naver.com>
2014-04-11 19:21:27 +0900

    파일경로에 \에러뜨는거 처리

--- Pooptube/CoreEngine/LibSet.h
+++ Pooptube/CoreEngine/LibSet.h
@@ -4,12 +4,14 @@
 #pragma comment(lib, "winmm.lib") // timeGetTime 지우면 필요없을걸 아마 chrono로 바꾸면 삭제합시다
 
 // d3d
-#pragma comment(lib, "C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Lib\x86\d3d9.lib")
+#pragma comment(lib, "C:\\Program Files (x86)\\Microsoft DirectX SDK (June 2010)\\Lib\\x86\\d3d9.lib")
 
 #ifdef _DEBUG
-#pragma comment(lib, "C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Lib\x86\d3dx9d.lib")
-#pragma comment(lib, "C:\Program Files\Autodesk\FBX\FBX SDK\2014.2.1\lib\vs2012\x86\debug\libfbxsdk.lib")
+#pragma comment(lib, "C:\\Program Files (x86)\\Microsoft DirectX SDK (June 2010)\\Lib\\x86\\d3dx9d.lib")
+#pragma comment(lib, "C:\\Program Files\\Autodesk\\FBX\\FBX SDK\\2014.2.1\\lib\\vs2012\\x86\\debug\\libfbxsdk.lib")
+
 #else
-#pragma comment(lib, "C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Lib\x86\d3dx9.lib")
-#pragma comment(lib, "C:\Program Files\Autodesk\FBX\FBX SDK\2014.2.1\lib\vs2012\x86\release\libfbxsdk.lib")
+#pragma comment(lib, "C:\\Program Files (x86)\\Microsoft DirectX SDK (June 2010)\\Lib\\x86\\d3dx9.lib")
+#pragma comment(lib, "C:\\Program Files\\Autodesk\\FBX\\FBX SDK\\2014.2.1\\lib\\vs2012\\x86\\release\\libfbxsdk.lib")
+
 #endif
