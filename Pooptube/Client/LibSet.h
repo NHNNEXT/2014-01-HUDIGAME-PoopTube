6418ce2d59c05b85171d9a6df349ab1c0e897934
양현찬 <e7315126@naver.com>
2014-04-12 18:37:48 +0900

    클라이언트 프로젝트 내용추가

--- Pooptube/Client/LibSet.h
+++ Pooptube/Client/LibSet.h
@@ -1,1 +1,12 @@
 #pragma once
+
+// d3d
+#pragma comment(lib, "C:\\Program Files (x86)\\Microsoft DirectX SDK (June 2010)\\Lib\\x86\\d3d9.lib")
+
+#ifdef _DEBUG
+#pragma comment(lib, "C:\\Program Files (x86)\\Microsoft DirectX SDK (June 2010)\\Lib\\x86\\d3dx9d.lib")
+
+#else
+#pragma comment(lib, "C:\\Program Files (x86)\\Microsoft DirectX SDK (June 2010)\\Lib\\x86\\d3dx9.lib")
+
+#endif
--- Pooptube/Client/StageOne.h
+++ Pooptube/Client/StageOne.h
@@ -1,8 +1,62 @@
 #pragma once
+#include "Scene.h"
+#include "Camera.h"
+
+using namespace pooptube;
+
 class StageOne
+	: public Scene
 {
 public:
 	StageOne();
 	~StageOne();
+
+	bool Init();
+
+	static StageOne* Create();
+
+	void Render() {
+		mCamera->Render();
+	}
+
+	void Update(float dTime) {
+	}
+
+	void KeyDown(KeyEvent* pKeyEvent) {
+		printf("KeyDown\n");
+	}
+	void KeyPressed(KeyEvent* pKeyEvent) {
+		printf("KeyPressed\n");
+	}
+	void KeyUp(KeyEvent* pKeyEvent) {
+		printf("KeyUp\n");
+	}
+
+	void MouseDown(MouseEvent* pMouseEvent) {
+		printf("MouseDown\n");
+	}
+
+	void MouseMove(MouseEvent* pMouseEvent) {
+		printf("MouseMove %d %d\n", pMouseEvent->GetX(), pMouseEvent->GetY());
+	}
+
+	void MouseUp(MouseEvent* pMouseEvent) {
+		printf("MouseUp\n");
+	}
+
+	void MousePressed(MouseEvent* pMouseEvent) {
+		printf("MousePressed\n");
+	}
+
+	void MouseWheel(MouseEvent* pMouseEvent) {
+		printf("MouseWheel\n");
+	}
+
+	
+
+protected:
+
+private:
+	Camera* mCamera;
 };
 
