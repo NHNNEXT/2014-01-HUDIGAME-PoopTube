4cc3791302f68c75717c7ed0e7cec452122802a2
mooneegee <mooneegee@naver.com>
2014-04-22 04:58:12 +0900

    Config.h <- IncludeSet.h, LibSet.h, Macro.h

---
+++ Pooptube/CoreEngine/Config.h
@@ -0,0 +1,20 @@
+/**
+* Config.h
+* 작성자: 장문익
+* 작성일: 2014. 4. 22
+* 마지막으로 수정한 사람: 장문익
+* 수정일: 2014. 4. 22
+*/
+
+#pragma once
+
+#pragma warning(disable:4005)
+
+//#include "NNIncludeSet.h"
+#include "LibSet.h"
+#include "MacroSet.h"
+
+/* */
+/* NNConfig
+/* 인클루드 헤더와 라이브러리, 매크로 설정이 모인 헤더파일
+/* */
