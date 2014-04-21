d3e499c2f5e0cf27b83af5c47653c0049dc2c6f3
mooneegee <mooneegee@naver.com>
2014-04-22 02:31:10 +0900

    MacroSet.h Singleton Macro added

--- Pooptube/CoreEngine/MacroSet.h
+++ Pooptube/CoreEngine/MacroSet.h
@@ -1,3 +1,36 @@
 
 #pragma once
 
+#define ALIGNMENT_SIZE 16
+
+template <class T>
+class Singleton
+{
+protected:
+	Singleton(void) {}
+	virtual ~Singleton(void) {}
+public:
+	static T* GetInstance(void)
+	{
+		if (!m_Instance)
+		{
+			m_Instance = (T*)_aligned_malloc(sizeof(T), ALIGNMENT_SIZE);
+			new (m_Instance)T();
+		}
+		return m_Instance;
+	}
+
+	static void Release(void)
+	{
+		if (m_Instance)
+		{
+			m_Instance->~T();
+			_aligned_free(m_Instance);
+		}
+	}
+
+private:
+	static T*	m_Instance;
+};
+template <typename T>
+T* Singleton<T>::m_Instance = nullptr;
