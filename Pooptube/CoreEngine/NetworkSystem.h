47699560a71273eeb7fe8cd5cee377f70b3c7310
mooneegee <mooneegee@naver.com>
2014-04-22 05:08:58 +0900

    NetworkSystem.h upload

--- Pooptube/CoreEngine/NetworkSystem.h
+++ Pooptube/CoreEngine/NetworkSystem.h
@@ -1,2 +1,42 @@
 
 #pragma once
+
+#include "Config.h"
+#include "PacketHeader.h"
+#include "CircularBuffer.h"
+#include "BaseHandler.h"
+#include <map>
+
+namespace pooptube {
+	class NetworkSystem : public Singleton<NetworkSystem> {
+	public:
+		NetworkSystem();
+		~NetworkSystem();
+
+		bool Init();
+		void Destroy();
+
+		bool Connect(const char* serverIP, int port);
+		void Disconnect();
+
+		void SetPacketHandler(short packetType, BaseHandler* handler);
+
+		void Write(const char* data, size_t size);
+		void Read();
+
+	private:
+		void ProcessPacket();
+
+	private:
+		SOCKET		mSocket;
+		SOCKADDR_IN	mServerAddr;
+
+		char*		mServerIP;
+		int			mPort;
+
+		std::map<short, BaseHandler*> mPacketHandler;
+
+		CircularBuffer	mRecvBuffer;
+		CircularBuffer	mSendBuffer;
+	};
+}
