81099f546a79bfb3c3081ff182cc9144e0417579
양현찬 <e7315126@naver.com>
2014-04-24 16:23:32 +0900

    HeightMap클래스 다시 추가

---
+++ Pooptube/CoreEngine/HeightMap.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "HeightMap.h"
+
+
+HeightMap::HeightMap()
+{
+}
+
+
+HeightMap::~HeightMap()
+{
+}
---
+++ Pooptube/CoreEngine/HeightMap.h
@@ -0,0 +1,8 @@
+#pragma once
+class HeightMap
+{
+public:
+	HeightMap();
+	~HeightMap();
+};
+
