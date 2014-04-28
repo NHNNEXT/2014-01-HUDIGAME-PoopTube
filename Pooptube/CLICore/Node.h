f41adb1291465d5c82743357979572c106e41a45
jihwankim <mastrayer@naver.com>
2014-04-28 21:51:37 +0900

    # CLI compile error fix

---
+++ Pooptube/CLICore/Node.h
@@ -0,0 +1,20 @@
+#pragma once
+#pragma comment(lib, "CoreEngine")
+#include "..\CoreEngine\Node.h"
+
+using namespace System;
+
+namespace Core {
+
+	public ref class Character
+	{
+// 	protected:
+// 		pooptube::Node *pInstance = nullptr;
+// 
+// 	public:
+// 		void AddChild(Node ^pChild) { this->pInstance->AddChild(pChild); }
+// 		void RemoveChild(Node ^pChild) { }
+// 		Node() : pInstance(pooptube::Application::GetInstance()) { }
+// 		~Node() { pInstance->Release(); }
+	};
+}
