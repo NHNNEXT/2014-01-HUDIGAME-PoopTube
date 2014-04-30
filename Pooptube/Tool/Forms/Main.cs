a233c9bc1a46ae9a09c4c7daa8c60b080aed8590
jihwankim <mastrayer@naver.com>
2014-05-01 05:28:17 +0900

    # tool add Light

--- Pooptube/Tool/Forms/Main.cs
+++ Pooptube/Tool/Forms/Main.cs
@@ -18,10 +18,21 @@ namespace Tool
         {
             InitializeComponent();
 
+            Core.Scene Scene = null;
+            Core.Ground Ground = null;
+            Core.SunLight SunLight = null;
+
+             
             Application.init(this.ViewBox.Handle.ToInt32(), this.Size.Height, this.Size.Width);
+            Scene = new Core.Scene();
+            Ground = new Core.Ground();
+            SunLight = new Core.SunLight();
+
+            Application.ChangeScene(Scene);
+            Scene.AddChild(Ground);
+            Scene.AddChild(SunLight);
 
             Run();
-            
         }
 
         private async void Run()
