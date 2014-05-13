ba52548a7e894b78c1bd2099db9d921fdd7e6150
1기 조익성 <slowt@naver.com>
2014-05-13 19:55:56 +0900

    FMOD 사운드 시스템 추가

---
+++ Pooptube/CoreEngine/SoundBox.cpp
@@ -0,0 +1,54 @@
+﻿#include "stdafx.h"
+#include "SoundBox.h"
+#include "CollisionBox.h"
+#include "CollisionManager.h"
+
+namespace pooptube {
+	SoundBox::SoundBox()
+	{
+	}
+
+	SoundBox::~SoundBox()
+	{
+		mSound->release();
+	}
+
+	SoundBox * SoundBox::Create( FMOD::Studio::EventInstance* soundInstance )
+	{
+		SoundBox *pSoundBox( new SoundBox );
+
+		if( pSoundBox->Init( soundInstance ) ){
+			return pSoundBox;
+		}
+		else
+			return nullptr;
+	}
+
+	bool SoundBox::Init( FMOD::Studio::EventInstance* soundInstance )
+	{
+		Node::Init();
+		mSound = soundInstance;
+		return true;
+	}
+
+	void SoundBox::Update( float dTime )
+	{
+		Node::Update( dTime );
+
+// 		mCollisionBox->SetPosition( Node::GetPosition() );
+// 		mCollisionBox->Translation( D3DXVECTOR3( 0.f, mCollisionBox->GetAxisLenY(), 0.f ) );
+// 		mCollisionBox->SetFrontVector( Node::GetFrontVector() );
+// 		mCollisionBox->Update( dTime );
+		CollisionBox* collisionResult = pooptube::CollisionManager::GetInstance()->CollisionCheckNode( this );
+		if( collisionResult == nullptr || collisionResult->GetCollisionType() != CollisionBox::COLLISION_TYPE::PLAYER ){
+			mSound->stop( FMOD_STUDIO_STOP_ALLOWFADEOUT );
+		}
+		else{
+			FMOD_STUDIO_PLAYBACK_STATE playBackState;
+			mSound->getPlaybackState( &playBackState );
+			if( playBackState != FMOD_STUDIO_PLAYBACK_PLAYING)
+				mSound->start();
+		}
+	}
+
+}
