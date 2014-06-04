aa811aee5f26230105441fc25cbd9dc09505d6e1
1기 조익성 <slowt@naver.com>
2014-06-05 06:04:56 +0900

    빌보드 : 텍스쳐 uv transform

--- Pooptube/CoreEngine/BillBoard.cpp
+++ Pooptube/CoreEngine/BillBoard.cpp
@@ -3,8 +3,8 @@
 #include "ResourceManager.h"
 
 namespace pooptube{
-	const int VERTEX_NUM = 4;
-	const int POLYGON_NUM = 2;
+	const int BILLBOARD_VERTEX_NUM = 4;
+	const int BILLBOARD_POLYGON_NUM = 2;
 
 	BillBoard::BillBoard()
 	{
@@ -26,17 +26,9 @@ namespace pooptube{
 	bool BillBoard::_Init()
 	{
 		Node::Init();
-		//버텍스 버퍼 생성
-		if( GetDevice()->CreateVertexBuffer( VERTEX_NUM * sizeof(MESH_CUSTOM_VERTEX ),
-			0, D3DFVF_CUSTOMVERTEX,
-			D3DPOOL_DEFAULT, &mMeshVertexBuffer, NULL) < 0)
-		{
-			return false;
-		}
 
-		VOID* pVertices;
-		if( mMeshVertexBuffer->Lock( 0, VERTEX_NUM * sizeof(MESH_CUSTOM_VERTEX), (void**)&pVertices, 0 ) < 0 )
-			return false;
+		D3DXMatrixIdentity( &mMatIdentity );
+		D3DXMatrixIdentity( &mMatrix );
 
 		vertexList[0] = {
 			D3DXVECTOR3( -1.f, 1.f, 0.f ),
@@ -62,25 +54,22 @@ namespace pooptube{
 			D3DXCOLOR( 1.f, 1.f, 1.f, 1.f ),
 			1.f, 0.f
 		};
-		memcpy( pVertices, vertexList.data(), VERTEX_NUM * sizeof(MESH_CUSTOM_VERTEX) );
-		mMeshVertexBuffer->Unlock();
 
-		//인덱스 버퍼 생성
-		if( GetDevice()->CreateIndexBuffer( POLYGON_NUM * sizeof(MESH_CUSTOM_INDEX), 0, D3DFMT_INDEX32,
-			D3DPOOL_DEFAULT, &mMeshIndexBuffer, NULL) < 0)
+		//버텍스 버퍼 생성
+		if( GetDevice()->CreateVertexBuffer( BILLBOARD_VERTEX_NUM * sizeof( MESH_CUSTOM_VERTEX ), 0, D3DFVF_CUSTOMVERTEX, D3DPOOL_DEFAULT, &mMeshVertexBuffer, NULL ) < 0 )
 		{
 			return false;
 		}
-
-		VOID* pIndices;
-		if( mMeshIndexBuffer->Lock( 0, POLYGON_NUM * sizeof(MESH_CUSTOM_INDEX), (void**)&pIndices, 0 ) < 0 )
+		if(!_SetVertexBuffer())
 			return false;
-		std::array<MESH_CUSTOM_INDEX, POLYGON_NUM> indexList;
-		indexList[0] = { 0, 3, 1 };
-		indexList[1] = { 3, 2, 1 };
-		memcpy( pIndices, indexList.data(), POLYGON_NUM * sizeof(MESH_CUSTOM_INDEX) );
-		mMeshIndexBuffer->Unlock();
 
+		//인덱스 버퍼 생성
+		if( GetDevice()->CreateIndexBuffer( BILLBOARD_POLYGON_NUM * sizeof( MESH_CUSTOM_INDEX ), 0, D3DFMT_INDEX32, D3DPOOL_DEFAULT, &mMeshIndexBuffer, NULL ) < 0 ) {
+			return false;
+		}
+		if(!_SetIndexBuffer())
+			return false;
+		
 		return true;
 	}
 
@@ -102,16 +91,54 @@ namespace pooptube{
 			//인덱스 설정
 			mDevice->SetIndices(mMeshIndexBuffer);
 
-			GetDevice()->SetTexture(0, mTexture);
-			mDevice->DrawIndexedPrimitive(D3DPT_TRIANGLELIST, 0, 0, VERTEX_NUM, 0, POLYGON_NUM);
+			mDevice->SetTextureStageState( 0, D3DTSS_TEXTURETRANSFORMFLAGS, D3DTTFF_COUNT2 );
+			mDevice->SetTransform( D3DTS_TEXTURE0, &mMatrix );
+
+			GetDevice()->SetTexture( 0, mTexture );
+			
+			mDevice->DrawIndexedPrimitive( D3DPT_TRIANGLELIST, 0, 0, BILLBOARD_VERTEX_NUM, 0, BILLBOARD_VERTEX_NUM );
+			mDevice->SetTransform( D3DTS_TEXTURE0, &mMatIdentity );
 		}
-		else
-			return;
 	}
 
 	void BillBoard::SetTexture( std::wstring filename )
 	{
 		mTexture = ResourceManager::GetInstance()->LoadTexture( filename );
+	}
+
+	void BillBoard::SetTextureUVSize( float u, float v )
+	{
+		vertexList[1].tv = v;
+		vertexList[2].tu = u;
+		vertexList[2].tv = v;
+		vertexList[3].tu = u;
+		_SetVertexBuffer();
+	}
+
+	bool BillBoard::_SetVertexBuffer()
+	{
+		VOID* pVertices;
+		if( mMeshVertexBuffer->Lock( 0, BILLBOARD_VERTEX_NUM * sizeof( MESH_CUSTOM_VERTEX ), (void**)&pVertices, 0 ) < 0 )
+			return false;
+		memcpy( pVertices, vertexList.data(), BILLBOARD_VERTEX_NUM * sizeof( MESH_CUSTOM_VERTEX ) );
+		mMeshVertexBuffer->Unlock();
+
+		return true;
+	}
+
+	bool BillBoard::_SetIndexBuffer()
+	{
+		std::array<MESH_CUSTOM_INDEX, BILLBOARD_POLYGON_NUM> indexList;
+		indexList[0] = { 0, 3, 1 };
+		indexList[1] = { 3, 2, 1 };
+
+		VOID* pIndices;
+		if( mMeshIndexBuffer->Lock( 0, BILLBOARD_POLYGON_NUM * sizeof( MESH_CUSTOM_INDEX ), (void**)&pIndices, 0 ) < 0 )
+			return false;
+		memcpy( pIndices, indexList.data(), BILLBOARD_POLYGON_NUM * sizeof( MESH_CUSTOM_INDEX ) );
+		mMeshIndexBuffer->Unlock();
+
+		return true;
 	}
 
 }
--- Pooptube/CoreEngine/BillBoard.h
+++ Pooptube/CoreEngine/BillBoard.h
@@ -16,11 +16,17 @@ namespace pooptube{
 		virtual void Render();
 
 		void SetTexture( std::wstring filename );
+		void SetTextureUVSize( float u, float v );
+		void SetTextureMatrix( D3DXMATRIXA16& matMatrix ){ mMatrix = matMatrix; }
 
 	private:
+		bool _SetVertexBuffer();
+		bool _SetIndexBuffer();
+
 		LPDIRECT3DVERTEXBUFFER9		mMeshVertexBuffer = nullptr;
 		LPDIRECT3DINDEXBUFFER9		mMeshIndexBuffer = nullptr;
 		LPDIRECT3DTEXTURE9			mTexture = nullptr;
+		D3DXMATRIXA16				mMatrix, mMatIdentity;
 
 		std::array<MESH_CUSTOM_VERTEX, 4> vertexList;
 	};
